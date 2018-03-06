f7adfae3cbd9c10fe4f0c16386bd69e33f2115b5
Jesús Alfredo Reyes Vargas <jesus.reyes@unet.edu.ve>
2018-03-05 21:48:10 -0400

    Añade la consulta para sacar el hecho 1 (falta generar la funciónque extraerá la cantidad de esperas [coming soon])

--- consultas-extraccion-hecho.sql
+++ consultas-extraccion-hecho.sql
@@ -1,3 +1,25 @@
+-- HECHO #1: CANTIDA, GANANCIAS Y ESPERAS GENERADAS POR UNA PELICULA ALQUILADA EN UNA TIENDA ESPECIFICA EN UN DIA DADO POR UN EMPLEADO EN ESPECIFICO
+
+select 
+	rent.rental_date::date as "date",
+	sto.store_id,
+	st.staff_id,
+	f.film_id,
+	count(*) as quantity,
+	sum(pay.amount) as revenue,
+	avg(rent.return_date - rent.rental_date) as avg_time,
+	calculate_wait_quantity( rent.rental_date::date as "date", sto.store_id, st.staff_id, f.film_id ) as wait_quantity
+from rental rent
+join customer cust on rent.customer_id = cust.customer_id
+join inventory inv on rent.inventory_id = inv.inventory_id
+join film f on f.film_id = inv.inventory_id
+join staff st on rent.staff_id = st.staff_id
+join "store" sto on st.store_id = sto.store_id
+join payment pay on rent.rental_id = pay.rental_id
+--group by rent.rental_id
+group by rent.rental_date, f.film_id, st.staff_id, sto.store_id 
+order by "date", store_id, staff_id, film_id
+
 -- HECHO # 2, CANTIDAD, GANANCIAS Y TIEMPO DE ALQUILER PROMEDIO DE PELICULAS POR CATEGORIA PARA CADA CLIENTE EN CADA TIENDA
 
 select 
